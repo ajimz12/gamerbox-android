0c0d8a3316f0f0dab43922903fde6ef5f3984d14
Alvaro <alvarojimgil12@gmail.com>
2024-05-04 19:03:04 +0200

    REFACTOR: Se muestran los titulos de los juegos

--- app/src/main/java/com/example/gamerbox/models/GameAdapter.kt
+++ app/src/main/java/com/example/gamerbox/models/GameAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.gamerbox.R
@@ -12,7 +13,8 @@ class GameAdapter(private val gamesList: List<Game>) :
     RecyclerView.Adapter<GameAdapter.GameViewHolder>() {
 
     class GameViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imageViewCover: ImageView = itemView.findViewById(R.id.gameImage)
+        val imageViewCover: ImageView = itemView.findViewById(R.id.gameImageView)
+        val textViewName: TextView = itemView.findViewById(R.id.gameTitleTextView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
@@ -23,6 +25,7 @@ class GameAdapter(private val gamesList: List<Game>) :
 
     override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
         val game = gamesList[position]
+        holder.textViewName.text = game.name
         Glide.with(holder.itemView.context)
             .load(game.backgroundImageUrl)
             .into(holder.imageViewCover)
