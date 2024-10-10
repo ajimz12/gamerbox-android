ca6c9bcfc3415eb42b40536bb681a1dec94c91f6
Álvaro <alvarojimgil12@gmail.com>
2024-10-11 01:28:53 +0200

    Update Constants.kt

--- app/src/main/java/com/example/gamerbox/utils/Constants.kt
+++ app/src/main/java/com/example/gamerbox/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.example.gamerbox.utils
 object Constants {
 
     const val BASE_URL = "https://api.rawg.io/api/"
-    const val API_KEY = "d45e7161cdba47c3bf28047a194fc92b"
+    const val API_KEY = ""
 
 }
